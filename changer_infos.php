5ecb43dd4179aaf4ae7b1a4dfa7a272abf881d17
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-03-24 14:52:29 +0100

    Update changer_infos.php

--- changer_infos.php
+++ changer_infos.php
@@ -2,6 +2,7 @@
 require('titre.html');
 require('connexion.php');
 
+//on récupere des infos de formulaire
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $datenaissance = $_POST['datenaiss'];
@@ -10,23 +11,24 @@ $tel = $_POST['tel'];
 $sexe = $_POST['sexe'];
 $motpass = $_POST['motpass'];
 
+//la mise à jour des informations personnelles
 $sqlupdate = "UPDATE client SET NomClient = '$nom', PrenomClient = '$prenom', DateNaissance = '$datenaissance', Email = '$email', Telephone = '$tel', Sexe = '$sexe', MotDePasse ='$motpass'WHERE NumClient = $numClient";
 $updated = $conn->query($sqlupdate);
 
+//l'affichage des informations personnelles actualisées
 $sql = "SELECT * FROM client WHERE NumClient = $numClient"; 
 $result = $conn->query($sql);
 
-
-    echo "<p style='color:red;'>! Vous avez bien change vos informations personnelles ! </p>";
-    echo "<h3><b>Infos actualisees: </b></h3>";
+    echo "<p style='color:red;'>! Vous avez bien changé vos informations personnelles ! </p>";
+    echo "<h3><b> Informations actualisées: </b></h3>";
     $row = $result->fetch_assoc();
-        echo "<div>";
         echo "<p> Prenom: " . $row["PrenomClient"] . "</p>";
         echo "<p> Nom: " . $row["NomClient"] . " </p>";
         echo "<p> Date de Naissance: ". $row["DateNaissance"]."</p>";
         echo "<p> E-mail: " . $row["Email"] . "</p>";
         echo "<p> Telephone: " . $row["Telephone"] . "</p>";
         echo "<p> Sexe: " . $row["Sexe"] . "</p>";
-
         echo "<button> <a href='profil.php?NumClient=$numClient'> Retourner au profil </a> </button>";
+
+$conn->close();
 ?>
