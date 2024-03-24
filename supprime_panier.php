3fcdeca999775eb4692e9c95d4da5ace9b402a3e
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-03-24 14:56:12 +0100

    Update supprime_panier.php

--- supprime_panier.php
+++ supprime_panier.php
@@ -11,16 +11,16 @@ if ($resultquantite->num_rows > 0) {
     $row = $resultquantite->fetch_assoc();
     $quantite = $row['Quantite'];
 
+    //On vérifie s'il faut supprimer un enregistrement ou diminuer la quantité des produits dans le panier
     if ($quantite >= 2) {
         $sqlu = "UPDATE panier_details SET Quantite = Quantite - 1 WHERE NumPanier = '$numClient' AND NumProduit = '$numProduit'";
         $resultu = $conn->query($sqlu);
-        echo "<p style='color:red;'>! Vous avez bien supprime l'article de votre panier !</p>";
+        echo "<p style='color:red;'>! Vous avez bien supprimé  l'article de votre panier !</p>";
     }
     else {
         $sqld = "DELETE FROM panier_details WHERE NumProduit = $numProduit AND NumPanier = $numClient ";
         $resultd = $conn->query($sqld);
-        echo "<p style='color:red;'>! Vous avez bien supprime l'article de votre panier !</p>";
-        
+        echo "<p style='color:red;'>! Vous avez bien supprimé  l'article de votre panier !</p>";  
     }
 }
 echo "<br>";
