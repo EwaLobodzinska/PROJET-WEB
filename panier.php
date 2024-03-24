f3c42bc344f6e6e622d238887d84a617de3f6d1d
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-03-24 14:54:33 +0100

    Update panier.php

--- panier.php
+++ panier.php
@@ -1,7 +1,9 @@
 <?php
-// Connexion à la base de données
+/*on a créé panier_info.php pour pouvoir afficher le panier 
+après l'ajout et la supression des articles de panier */
+
 require('titre.html');
 require('connexion.php');
 require('panier_info.php');
-$conn->close();
+
 ?>
