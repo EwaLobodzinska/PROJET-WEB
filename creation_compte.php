53a9d58087637edc99da29c978a8d088c41e117b
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-03-24 14:53:41 +0100

    Update creation_compte.php

--- creation_compte.php
+++ creation_compte.php
@@ -1,7 +1,7 @@
 <?php
 require('titre.html');
 
-//Connexion a la base de donnees
+//Connexion à la base de données
 $host = "localhost";
 $user = "root"; 
 $mdp = ""; 
@@ -9,6 +9,7 @@ $name = "vente en ligne";
 
 $conn = new mysqli($host, $user, $mdp, $name);
 
+//on récupere des informations de formulaire
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $datenaissance = $_POST['datenaiss'];
@@ -17,21 +18,24 @@ $tel = $_POST['tel'];
 $sexe = $_POST['sexe'];
 $motpass = $_POST['motpass'];
 
-$sql = "INSERT INTO client (NomClient, PrenomClient, DateNaissance, Email, Telephone, Sexe, MotDePasse) VALUES ('$nom', '$prenom', '$datenaissance', '$email','$tel', '$sexe', '$motpass')";
+$sql = "INSERT INTO client (NomClient, PrenomClient, DateNaissance, Email, Telephone, Sexe, MotDePasse) 
+        VALUES ('$nom', '$prenom', '$datenaissance', '$email','$tel', '$sexe', '$motpass')";
 
 if ($conn->query($sql) === TRUE) {
     $numClient = $conn->insert_id;
     $sqlpanier = "INSERT INTO panier (NumPanier, NumClient) VALUES ('$numClient', '$numClient')";
     $resultpanier = $conn->query($sqlpanier);
-    echo "Votre compte etait bien cree <br> votre numero de client est:".$numClient;
-    echo "<br> Inserer votre numero de client pour voir des articles et continuer";
-    echo "<form action='produits.php' method='POST'";
-    echo "<label id='NumClient'> Numero de client  </label>";
-    echo "<input type='text' id='NumClient' name='NumClient'>";
-    echo "<button> Voir articles </button>";
+    echo "<p style='color:red;'>Votre compte a bien été créé. </p> Votre numéro de client est: <b>".$numClient. "</b>";
+    echo "<br> Insérez votre numéro de client et mot de passe pour continuer: <br>";
+    echo "<form action='connexion_compte.php' method='POST'";
+    echo "<label id='NumClient'> Numéro de client  </label>";
+    echo "<input type='text' id='NumClient' name='NumClient'> <br>";
+    echo "<label id='motpasse'>Entrez votre mot de passe</label>";
+    echo "<input type='password' id='motpasse' name='motpasse'>";
+    echo "<button> Continuer </button>";
     echo "</form>";
 } else {
-    echo "Erreur : " . $sql . "<br>" . $conn->error;
+    echo "Erreur : ".$sql."<br>".$conn->error;
 }
 
 $conn->close();
