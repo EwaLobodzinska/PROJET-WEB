b55f72c71f85629df83b9e883d894dc5603a9c91
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-23 19:05:13 +0200

    Update connexion_compte.php

--- connexion_compte.php
+++ connexion_compte.php
@@ -1,22 +1,36 @@
-<?php
-require('titre.html');
+<?php #LOBODZINSKA et NGUYEN
+require('titre.html'); #on exige le fichier 'title.html' qui contient l'apparence graphique du site Web
 require('connexion.php');
 
-$sqlclient = "SELECT * FROM client WHERE NumClient = $numClient";
-$resultclient = $conn->query($sqlclient);
 
-$motpasse=$_POST['motpasse'];
-$row = $resultclient->fetch_assoc();
+if (isset($_POST['email']) and isset($_POST['motpasse'])) {
+    #on récupère les données du formulaire HTML
+    $mail = $_POST["email"];
+    $mdp = $_POST["motpasse"];
+    $sqlclient = "SELECT NumClient, Email, MotDePasse FROM client WHERE Email='$mail'";
+    $resultclient = $conn->query($sqlclient);
+    $row = $resultclient->fetch_row();
+    # Vérification de l'existence d'un compte associé à l'adresse mail entrée
+    if ($resultclient->num_rows > 0) {
+        # Vérification du mot de passe
+        if ($mdp == $row[2]) {
+            echo "<p style='color: rgb(45, 29, 86);'>Vous êtes bien connectés ! Pour continuer, cliquez sur le bouton :)</p><br>" .
+                "<button><a href='produits.php'>Voir des articles</a></button>";
+            #Pour toujours avoir l'identité du client enregistré, on utilise la superglobale SESSION
+            session_start();
+            $_SESSION['email'] = $mail;
+            $_SESSION['motpasse'] = $mdp;
+            $_SESSION['num'] = $row[0];
 
-//on vérifie si le client existe et le mot de passe est correct
-if ($resultclient->num_rows > 0 AND $row['MotDePasse'] == $motpasse) {
-    echo "<p style='color:rgb(45, 29, 86);'> Vous êtes bien connecté! Pour continuer, cliquez sur le bouton :) </p><br>";
-    echo "<button> <a href='produits.php?NumClient=$numClient'> Voir des articles </a> </button>";
-
-} else {
-    echo "<p style='color:red;'>Erreur: Client non trouvé ou mot de passe incorrect </p><br>";
-    echo "<button> <a href='accueil.html'> Revenir à  la page d'accueil </a> </button>";
+        } else {
+            echo "<p style='color: red;'>Mot de passe incorrect.</p><br>".
+                "<button><a href='accueil.html'>Revenir à la page d'accueil</a></button>";
+        }
+    } else {
+        echo "<p style='color: red;'>Aucun compte n'est associé à cette adresse mail.</p><br>"
+            . "<button><a href='accueil.html'>Revenir à la page d'accueil</a></button>";
+    }
 }
 
-$conn->close();
+$conn->close(); #Déconnexion de la base de données
 ?>
