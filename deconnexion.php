5e425af42dfd7297be909de85aacdfa5090af344
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-23 19:35:30 +0200

    Create deconnexion.php

---
+++ deconnexion.php
@@ -0,0 +1,12 @@
+<?php #LOBODZINSKA et NGUYEN
+require ('titre.html'); #on exige le fichier 'title.html' qui contient l'apparence graphique du site Web
+session_start();
+if (isset($_SESSION['email']) and isset($_SESSION['motpasse']) and isset($_SESSION['num'])) {
+    unset($_SESSION['email'], $_SESSION['motpasse'], $_SESSION['num']);
+    session_destroy();
+    if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['num'])) {
+        echo "Déconnexion réussie !<br>" .
+            "<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
+    }
+}
+?>
