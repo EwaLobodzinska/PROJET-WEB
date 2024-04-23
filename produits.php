34f94041ef5d152c85f133531701e4c7bbe0c3be
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-23 19:16:26 +0200

    Update produits.php

--- produits.php
+++ produits.php
@@ -1,35 +1,28 @@
-<?php
+<?php #LOBODZINSKA et NGUYEN
 require('titre.html');
 require('connexion.php');
 
-$sqlclient = "SELECT * FROM client WHERE NumClient = $numClient";
-$resultclient = $conn->query($sqlclient);
-
-echo "<h1 style='color:rgb(45, 29, 86);'> Ici vous pouvez voir tous nos articles proposés </h1>";
-echo "<button> <a href='panier.php?NumClient=$numClient'> Voir panier </a> </button>";
-echo "<button> <a href='profil.php?NumClient=$numClient'> Voir profil </a> </button>";
-echo "<button> <a href='accueil.html'> Se déconnecter </a> </button>";
+echo "<h1 style='color: rgb(45, 29, 86);'>Ici vous pouvez voir tous nos articles proposés !</h1>".
+"<button><a href='panier.php' style='color: black'>Voir panier</a></button>".
+"<button><a href='profil.php' style='color: black'>Voir profil</a></button>".
+"<button><a href='deconnexion.php'>Se déconnecter</a></button>";
 
 $sql = "SELECT * FROM produit";
 $result = $conn->query($sql);
 
-// On vérifie s'il y a des articles a récupérer
+#On vérifie s'il y a des articles à récupérer
 if ($result->num_rows > 0) {
-    // On affiche chaque article récupéré
+    #On affiche chaque article récupéré
     while ($row = $result->fetch_assoc()) {
-        echo "<div>";
-            echo "<h2 style='color:rgb(29, 86, 29);'>".$row["NomProduit"]."</h2>";
-            echo "<img src='".$row["Photo"]."'>";
-            echo "<p><b> Prix: </b>".$row["Prix"]." euros </p>";
-            echo "<p><b> Catégorie: </b>".$row["Categorie"]."</p>";
-            echo "<p><b> Marque: </b>".$row["Marque"]."</p>";
-            echo "<form action='ajout_panier.php' method='POST'>";
-                //Pour pouvoir récupérer le numéro de client et produit à la page suivante on utilise: type='hidden'
-                echo "<input type='hidden' name='NumProduit' value='".$row["NumProduit"] . "'>";
-                echo "<input type='hidden' name='NumClient' value='".$numClient." '>";
-                echo "<button type='submit'>Ajouter au panier</button>";
-            echo "</form>";
-        echo "</div><br>";
+        echo "<div><h2 style='color: rgb(29, 86, 29);'>".$row["NomProduit"]."</h2>".
+            "<img src='".$row["Photo"]."'>".
+            "<p><b>Prix : </b>".$row["Prix"]." euros</p>".
+            "<p><b>Catégorie : </b>".$row["Categorie"]."</p>".
+            "<p><b>Marque : </b>".$row["Marque"]."</p>".
+            "<form action='ajout_panier.php' method='POST'>".
+            #Pour récupérer le numéro du produit à la page suivante on utilise : type='hidden'
+            "<input type='hidden' name='NumProduit' value='".$row["NumProduit"] . "'>".
+            "<button type='submit'>Ajouter au panier</button></form></div><br>";
         }
 } else {
     echo "Aucun article trouvé.";
