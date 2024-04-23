175c013090e877716dd52d7d73846314c6b5d37e
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-23 17:50:03 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,22 +1,9 @@
-<?php 
-//Connexion à la base de donnees
-$host = "localhost"; // Adresse de serveur MySQL
-$user = "root"; // Nom d'utilisateur MySQL
-$mdp = ""; // Mot de passe MySQL
-$name = "vente en ligne"; // Nom de base de données
+<?php #LOBODZINSKA et NGUYEN
+#Connexion à la base de données 'vente en ligne'
+$host = "localhost"; #Adresse de serveur MySQL
+$user = "root"; #Nom d'utilisateur MySQL
+$mdp = ""; #Mot de passe MySQL
+$name = "vente en ligne"; #Nom de la base de données associé au site
 
-$conn = new mysqli($host, $user, $mdp, $name);
-
-//pour toujours avoir le numéro de client enregistré, on va utiliser la commande SESSION 
-session_start();
-if(isset($_POST['NumClient'])) {
-    $_SESSION['NumClient'] = $_POST['NumClient'];
-} elseif(isset($_GET['NumClient'])) {
-    $_SESSION['NumClient'] = $_GET['NumClient'];
-} else {
-    echo "Erreur : NumClient non défini.";
-}
-
-//on affecte le valeur à $numClient pour après pouvoir le facielemnt utiliser 
-$numClient = $_SESSION['NumClient'];
+$conn = new mysqli($host, $user, $mdp, $name); #Connexion
 ?>
