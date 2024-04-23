30939c9eaa52248023725f12a09b5c86550a3a34
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-23 22:14:40 +0200

    Update ajout_panier.php

--- ajout_panier.php
+++ ajout_panier.php
@@ -1,24 +1,25 @@
-<?php
+<?php #LOBODZINSKA et NGUYEN
 require('titre.html');
 require('connexion.php');
 
-$numProduit = $_POST['NumProduit'];
+if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])) {
+    session_start();
+}
 
-$sqlquantite = "SELECT * FROM panier_details WHERE NumPanier = $numClient AND NumProduit = $numProduit";
+$sqlquantite = "SELECT * FROM panier_details WHERE NumPanier = {$_SESSION['numclient']} AND NumProduit = {$_POST['NumProduit']}";
 $resultquantite = $conn->query($sqlquantite);
 
-//On vérifie s'il faut ajouter un enregistrement ou augementer la quantité des produits dans le panier
+#On vérifie s'il faut ajouter un enregistrement ou augmenter la quantité de produit dans le panier
 if ($resultquantite->num_rows > 0) {
-    $sqlu = "UPDATE panier_details SET Quantite = Quantite + 1 WHERE NumPanier = '$numClient' AND NumProduit = '$numProduit'";
+    $sqlu = "UPDATE panier_details SET Quantite = Quantite + 1 WHERE NumPanier = {$_SESSION['numclient']} AND NumProduit = {$_POST['NumProduit']}";
     $resultp = $conn->query($sqlu);
-    echo "<p style='color:red;'> ! Vous avez bien ajouté l'article à votre panier ! </p>";
+    echo "<p style='color: red;'>Vous avez bien ajouté l'article à votre panier !</p>";
 }
 else {
-    $sqli = "INSERT INTO panier_details (NumPanier, NumProduit, Quantite) VALUES ('$numClient', '$numProduit', '1') ";
+    $sqli = "INSERT INTO panier_details (NumPanier, NumProduit, Quantite) VALUES ({$_SESSION['numclient']}, {$_POST['NumProduit']}, 1)";
     $result = $conn->query($sqli);
-    echo "<p style='color:red;'> ! Vous avez bien ajouté l'article à votre panier ! </p>";
+    echo "<p style='color: red;'>Vous avez bien ajouté l'article à votre panier !</p><br>";
 }
-echo "<br>";
 
 require('panier_info.php');
 ?>
