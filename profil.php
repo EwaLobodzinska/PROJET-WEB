93a3d35e2e6446c51478c42fdbade303b7a6dcdd
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-23 22:53:21 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -16,44 +16,50 @@ echo "<button><a href='produits.php' style='color : black'>Voir des articles</a>
 ?>
 
 <html>
+    
     <body>
-    <br>
-        <form action="changer_infos.php" method="POST" style="background-color:#b6bfa2;">
+        <br>
+        <form action="changer_infos.php" method="POST" style="background-color: #b6bfa2;">
             <fieldset>
-            <legend> <b>Changer vos informations personnelles </b></legend>
-            <label id="prenom"> Votre prenom </label>
-            <input type ="text" id="prenom" name="prenom" value="<?php echo $row['PrenomClient']; ?>" >
-            <br>
-            <label id="nom"> Votre nom </label>
-            <input type ="text" id="nom" name="nom" value="<?php echo $row['NomClient']; ?>">
-            <br>
-            <label id="datenaiss"> Votre date de naissance </label>
-            <input type ="date" id="datenaiss" name="datenaiss" value="<?php echo $row['DateNaissance']; ?>">
-            <br>
-            <label id="email"> Votre adresse mail </label>
-            <input type ="text" id="email" name="email" value="<?php echo $row['Email']; ?>">
-            <br>
-            <label id="tel"> Votre numéro de telephone </label>
-            <input type ="text" id="tel" name="tel" value="<?php echo $row['Telephone']; ?>">
-            <br>
-            <label id="sexe"> Votre sexe </label>
-            <select name="sexe">
-                <option value="femme" <?php if ($row['Sexe'] == "femme") echo "selected"; ?>> Femme </option>
-                <option value="homme" <?php if ($row['Sexe'] == "homme") echo "selected"; ?>> Homme </option>
-                <option value="autre" <?php if ($row['Sexe'] == "autre") echo "selected"; ?>> Autre </option>
-            </select>
-            <br>
-            <label id="motpass"> Votre mot de passe </label>
-            <input type ="password" id="motpass" name="motpass" value="<?php echo $row['MotDePasse']; ?>">
-            <input type='hidden' name='NumClient' value=' <?php echo $row["NumClient"]; ?> '>
-            <button> Changer </button>
+                <legend><b>Changer vos informations personnelles</b></legend>
+                <label id="prenom">Votre prénom</label>
+                <input type ="text" id="prenom" name="prenom" value="<?php echo $row['PrenomClient']; ?>" >
+                <br>
+                <label id="nom">Votre nom</label>
+                <input type ="text" id="nom" name="nom" value="<?php echo $row['NomClient']; ?>">
+                <br>
+                <label id="datenaiss">Votre date de naissance</label>
+                <input type ="date" id="datenaiss" name="datenaiss" value="<?php echo $row['DateNaissance']; ?>">
+                <br>
+                <label id="email">Votre adresse mail</label>
+                <input type ="text" id="email" name="email" value="<?php echo $row['Email']; ?>">
+                <br>
+                <label id="tel">Votre numéro de téléphone</label>
+                <input type ="text" id="tel" name="tel" value="<?php echo $row['Telephone']; ?>">
+                <br>
+                <label id="sexe">Votre sexe</label>
+                <select name="sexe">
+                    <option value="femme" <?php if ($row['Sexe'] == "femme") {
+                        echo "selected"; 
+                    } ?>>Femme</option>
+                    <option value="homme" <?php if ($row['Sexe'] == "homme") {
+                        echo "selected"; 
+                    } ?>>Homme</option>
+                    <option value="autre" <?php if ($row['Sexe'] == "autre") {
+                        echo "selected"; 
+                    } ?>>Autre</option>
+                </select>
+                <br>
+                <label id="motpass">Votre mot de passe</label>
+                <input type ="password" id="motpass" name="motpass" value="<?php echo $row['MotDePasse']; ?>">
+                <button>Changer</button>
             </fieldset>
         </form>
 
         <form action="supprime_compte.php" method="POST">
-            <input type='hidden' name='NumClient' value=' <?php echo $row["NumClient"]; ?> '>
+            <input type='hidden'>
             <br><br><br>
-            <button> supprimer compte </button>
+            <button>Supprimer mon compte</button>
         </form>
     </body>
 
