ea0f17e6826c0b0241d027e10250e005c8f1148e
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-04-26 18:18:44 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,11 +1,12 @@
 <?php #LOBODZINSKA et NGUYEN
 require ('titre.html'); #on exige le fichier 'title.html' qui contient l'apparence graphique du site Web
+
 session_start();
 if (isset($_SESSION['email']) and isset($_SESSION['motpasse']) and isset($_SESSION['numclient'])) {
     unset($_SESSION['email'], $_SESSION['motpasse'], $_SESSION['num']);
     session_destroy();
     if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])) {
-        echo "<p style='color: rgb(45, 29, 86);'>Déconnexion réussie !</p><br>" .
+        echo "<br><p style='color: rgb(45, 29, 86);'>Déconnexion réussie !</p><br>" .
             "<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
     }
 }
