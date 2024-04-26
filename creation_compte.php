0ae69809c01503e4c5c864ff80315ce12b247908
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-04-26 18:17:57 +0200

    Update creation_compte.php

--- creation_compte.php
+++ creation_compte.php
@@ -11,24 +11,30 @@ $tel = $_POST['tel'];
 $sexe = $_POST['sexe'];
 $motpass = password_hash($_POST['motpass'], PASSWORD_DEFAULT);
 
-#Requête SQL pour insérer un nouvel enregistrement dans la table client
-$sql = "INSERT INTO client (NumClient, NomClient, PrenomClient, DateNaissance, Email, Telephone, Sexe, MotDePasse) 
-        VALUES (NULL, '$nom', '$prenom', '$datenaissance', '$email','$tel', '$sexe', '$motpass')";
-$result = $conn->query($sql);
+#On verifie si email n'est pas associe a aucun compte
+$sqlemail = "SELECT Email FROM client WHERE Email = '$email' ";
+$resultemail = $conn->query($sqlemail);
 
-#On récupère le numéro client qui vient d'être auto incrémenté
-$numClient = $conn->insert_id;
-
-#On créé aussi un panier pour le client
-$sqlpanier = "INSERT INTO panier (NumPanier, NumClient) VALUES ('$numClient', '$numClient')";
-$resultpanier = $conn->query($sqlpanier);
-
-if ($result == TRUE AND $resultpanier == TRUE) {
-    echo "<p style='color: red;'>Votre compte a bien été créé.</p>".
-            "<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
+if($resultemail->num_rows == 0){ 
+        #Requête SQL pour insérer un nouvel enregistrement dans la table client
+        $sql = "INSERT INTO client (NumClient, NomClient, PrenomClient, DateNaissance, Email, Telephone, Sexe, MotDePasse) 
+                VALUES (NULL, '$nom', '$prenom', '$datenaissance', '$email','$tel', '$sexe', '$motpass')";
+        $result = $conn->query($sql);
+        
+        #On récupère le numéro client qui vient d'être auto incrémenté
+        $numClient = $conn->insert_id;
+        
+        #On créé aussi un panier pour le client
+        $sqlpanier = "INSERT INTO panier (NumPanier, NumClient) VALUES ('$numClient', '$numClient')";
+        $resultpanier = $conn->query($sqlpanier);
+        
+        echo "<br><p style='color: red;'>Votre compte a bien été créé.</p>".
+            
 } else {
-    echo "Impossible de créer un compte";
+        echo "<br><p style='color: red;'>Impossible de créer un compte</p>";
 }
+
+echo "<br><button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
 
 $conn->close(); #Déconnexion de la base de données
 ?>
