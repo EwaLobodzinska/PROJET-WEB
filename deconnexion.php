7b363b34db24a001c34ef9e6b4db5daec437438b
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-27 21:43:42 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,13 +1,13 @@
 <?php #LOBODZINSKA et NGUYEN
 require ('titre.html'); #on exige le fichier 'title.html' qui contient l'apparence graphique du site Web
 
-if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient']))
-{session_start();}
+if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])){
+    session_start();
+}
 unset($_SESSION['email'], $_SESSION['motpasse'], $_SESSION['numclient']);
 session_destroy();
 if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])) {
 echo "<br><p style='color: rgb(45, 29, 86);'>Déconnexion réussie !</p><br>" .
-    "<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
-    }
+"<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
 }
 ?>
