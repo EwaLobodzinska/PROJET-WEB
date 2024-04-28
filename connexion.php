b21c727629c0f611778f686a9acaf394f885f7e1
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-28 14:18:03 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,9 +1,9 @@
 <?php #LOBODZINSKA et NGUYEN
-#Connexion à la base de données 'vente en ligne'
+
 $host = "localhost"; #Adresse de serveur MySQL
 $user = "root"; #Nom d'utilisateur MySQL
 $mdp = ""; #Mot de passe MySQL
 $name = "vente en ligne"; #Nom de la base de données associé au site
 
-$conn = new mysqli($host, $user, $mdp, $name); #Connexion
+$conn = new mysqli($host, $user, $mdp, $name); #La connexion est stockée dans la variable '$conn'.
 ?>
