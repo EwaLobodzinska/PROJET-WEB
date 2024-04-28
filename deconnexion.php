572840379c3f13d901c17d25ac04e9a9c0039bc0
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2024-04-28 10:33:30 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -3,11 +3,9 @@ require ('titre.html'); #on exige le fichier 'title.html' qui contient l'apparen
 
 if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])){
     session_start();
-}
-unset($_SESSION['email'], $_SESSION['motpasse'], $_SESSION['numclient']);
-session_destroy();
-if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])) {
-echo "<br><p style='color: rgb(45, 29, 86);'>Déconnexion réussie !</p><br>" .
-"<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
+    unset($_SESSION['email'], $_SESSION['motpasse'], $_SESSION['numclient']);
+    session_destroy();
+    echo "<br><p style='color: rgb(45, 29, 86);'>Déconnexion réussie !</p><br>" .
+    "<button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
 }
 ?>
