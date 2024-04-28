59ae21e08c728877c36d1f5377f96ce01ed58bbe
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-28 14:41:53 +0200

    Update creation_compte.php

--- creation_compte.php
+++ creation_compte.php
@@ -1,5 +1,5 @@
 <?php #LOBODZINSKA et NGUYEN
-require('titre.html'); #on exige le fichier 'title.html' qui contient l'apparence graphique du site Web
+require('titre.html'); 
 require ('connexion.php');
 
 #On récupere les informations du formulaire HTML
@@ -9,32 +9,31 @@ $datenaissance = $_POST['datenaiss'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
 $sexe = $_POST['sexe'];
-$motpass = password_hash($_POST['motpass'], PASSWORD_DEFAULT);
+$motpass = password_hash($_POST['motpass'], PASSWORD_DEFAULT); #On hache le mot de passe.
 
-#On verifie si email n'est pas associe a aucun compte
+#On vérifie si l'adresse mail n'est pas déjà associée à un compte.
 $sqlemail = "SELECT Email FROM client WHERE Email = '$email' ";
 $resultemail = $conn->query($sqlemail);
 
 if($resultemail->num_rows == 0){ 
-        #Requête SQL pour insérer un nouvel enregistrement dans la table client
+        #Requête SQL pour insérer un nouvel enregistrement dans la table 'client'
         $sql = "INSERT INTO client (NumClient, NomClient, PrenomClient, DateNaissance, Email, Telephone, Sexe, MotDePasse) 
                 VALUES (NULL, '$nom', '$prenom', '$datenaissance', '$email','$tel', '$sexe', '$motpass')";
         $result = $conn->query($sql);
         
-        #On récupère le numéro client qui vient d'être auto incrémenté
+        #On récupère le numéro client qui vient d'être auto incrémenté.
         $numClient = $conn->insert_id;
         
-        #On créé aussi un panier pour le client
+        #On créé aussi un panier pour le client avec le même numéro.
         $sqlpanier = "INSERT INTO panier (NumPanier, NumClient) VALUES ('$numClient', '$numClient')";
         $resultpanier = $conn->query($sqlpanier);
         
-        echo "<br><p style='color: red;'>Votre compte a bien été créé.</p>".
-            
+        echo "<br><p style='color: red;'>Votre compte a bien été créé.</p>".     
 } else {
         echo "<br><p style='color: red;'>Impossible de créer un compte</p>";
 }
 
 echo "<br><button><a href='accueil.html' style='color: black'>Revenir à la page d'accueil</a></button>";
 
-$conn->close(); #Déconnexion de la base de données
+$conn->close();
 ?>
