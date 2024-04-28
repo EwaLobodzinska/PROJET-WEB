773db42bdfeb64db5f8261c8006ca39a53b60258
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-28 16:58:12 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,5 +1,5 @@
 <?php #LOBODZINSKA et NGUYEN
-require ('titre.html'); #on exige le fichier 'title.html' qui contient l'apparence graphique du site Web
+require ('titre.html');
 
 if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])){
     session_start();
