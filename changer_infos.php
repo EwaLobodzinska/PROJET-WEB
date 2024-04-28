67c19e78b50736dc9519ef2cc281b1d073c337c8
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-28 17:05:12 +0200

    Update changer_infos.php

--- changer_infos.php
+++ changer_infos.php
@@ -3,11 +3,12 @@ require('connexion.php');
 require('titre.html');
 require ('menu.html');
 
+#On démarre la session si ce n'est pas déjà fait.
 if (!isset($_SESSION['email']) and !isset($_SESSION['motpasse']) and !isset($_SESSION['numclient'])) {
     session_start();
 }
 
-//on récupere des infos de formulaire
+#On récupère les informations du formulaire de modification du profil
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $datenaissance = $_POST['datenaiss'];
@@ -16,24 +17,23 @@ $tel = $_POST['tel'];
 $sexe = $_POST['sexe'];
 $motpass = password_hash($_POST['motpass'], PASSWORD_DEFAULT);
 
-//la mise à jour des informations personnelles
+#On met à jour les informations personnelles.
 $sqlupdate = "UPDATE client SET NomClient = '$nom', PrenomClient = '$prenom', DateNaissance = '$datenaissance', Email = '$email', Telephone = '$tel', Sexe = '$sexe', MotDePasse ='$motpass'WHERE NumClient = {$_SESSION['numclient']}";
 $updated = $conn->query($sqlupdate);
 
-//l'affichage des informations personnelles actualisées
+#On affiche les informations personnelles actualisées.
 $sql = "SELECT * FROM client WHERE NumClient = {$_SESSION['numclient']}"; 
 $result = $conn->query($sql);
-
-    echo "<p style='color:red;'>! Vous avez bien changé vos informations personnelles ! </p>";
-    echo "<h3><b> Informations actualisées: </b></h3>";
-    $row = $result->fetch_assoc();
-        echo "<p> Prenom: " . $row["PrenomClient"] . "</p>";
-        echo "<p> Nom: " . $row["NomClient"] . " </p>";
-        echo "<p> Date de Naissance: ". $row["DateNaissance"]."</p>";
-        echo "<p> E-mail: " . $row["Email"] . "</p>";
-        echo "<p> Telephone: " . $row["Telephone"] . "</p>";
-        echo "<p> Sexe: " . $row["Sexe"] . "</p>";
-        echo "<button><a href='profil.php'> Retourner au profil </a></button>";
+$row = $result->fetch_assoc();
+echo "<p style='color:red;'>! Vous avez bien changé vos informations personnelles ! </p>";
+"<h3><b> Informations actualisées : </b></h3>".
+"<p> Prénom : " . $row["PrenomClient"] . "</p>".
+"<p> Nom : " . $row["NomClient"] . " </p>".
+"<p> Date de Naissance : ". $row["DateNaissance"]."</p>".
+"<p> E-mail : " . $row["Email"] . "</p>".
+"<p> Téléphone : " . $row["Telephone"] . "</p>".
+"<p> Sexe : " . $row["Sexe"] . "</p>".
+"<button><a href='profil.php' style='color: black'> Retourner au profil </a></button>";
 
 $conn->close();
 ?>
