ec0457f7427d36794d675f0c9b0b278260c3de48
camviweb <167216844+camviweb@users.noreply.github.com>
2024-04-28 15:38:45 +0200

    Update panier.php

--- panier.php
+++ panier.php
@@ -1,10 +1,9 @@
 <?php #LOBODZINSKA et NGUYEN
 /*On a créé le fichier panier_info.php pour afficher le panier 
-après l'ajout ou la supression d'articles du panier*/
+après la supression d'articles*/
 
 require('connexion.php');
 require('titre.html');
 require ('menu.html');
 require('panier_info.php');
-
 ?>
